cfb2c11ba6f499e892add929da42321310448226
Luiz Gustavo Mesquita <lgustm@github.com>
2021-05-02 17:47:55 -0300

    Adicionei o Portifólio pronto

--- Portifólio/JS/portifolio.js
+++ Portifólio/JS/portifolio.js
@@ -1,6 +1,14 @@
-$(".btn-menu").click(function(){
-    $(".menu").show();
-});
-$(".btn-close").click(function(){
-    $(".menu").hide();
+var el = document.querySelector('.menu-mobile-icon');
+
+el.addEventListener('click', ()=>{
+    //show the menu.
+    var menuItems = document.querySelector('.menu-items');
+    if(menuItems.classList.contains('show')){
+        menuItems.classList.add('hide');
+        menuItems.classList.remove('show');
+    }
+    else{
+        menuItems.classList.add('show');
+        menuItems.classList.remove('hide');
+    }
 });
